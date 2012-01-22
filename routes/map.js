54d8e42140978adb0dd5754e2c92ef854e7e705c
irony <cl@iteam.se>
2012-01-22 20:39:57 +0100

    refactored some code

--- routes/map.js
+++ routes/map.js
@@ -7,42 +7,42 @@ module.exports = function(app){
 		var db = mongoose.connect('mongodb://localhost/graphTest');
 		
 		var Person = mongoose.model('Person', require('../models/person.js'));
+		var nodes = {};
+		var edges = {};
 		
-		Person.find({id : req.query['id'] || app.currentUserId}, function(err, docs){
-			var nodes = {};
-			var edges = {};
-			docs.forEach(function(person){
-				var company = getCompanyName(person);
-				if (!company)
-					return; // people with no companies will be sorted away here
-					
-				if (!nodes[company]) nodes[company] = {};			
-				if (!nodes[company].persons) nodes[company].persons = [];
+		var fillPerson = function(person, depth){
+			if (depth < 1)
+				return;
 				
-				nodes[company].persons.push({name : person.firstName + ' ' + person.lastName, id:person.id, position : person.company ? person.company.position : null});
+			var company = getCompanyName(person);
+			if (!company)
+				return; // people with no companies will be sorted away here
 				
-				if (person.friends.length > 0)
-				{
+			if (!nodes[company]) nodes[company] = {};			
+			if (!nodes[company].persons) nodes[company].persons = [];
+			
+			nodes[company].persons.push({name : person.firstName + ' ' + person.lastName, id:person.id, position : person.company ? person.company.position : null});
+			
+			if (person.friends.length > 0)
+			{
+				
+				person.friends.forEach(function(friend){
+					fillPerson(friend, depth-1);
+					if (!edges[company]) edges[company] = [];
+					edges[company].push(getCompanyName(friend));
 					
-					person.friends.forEach(function(friend){
-						var friendCompany = getCompanyName(friend);
-						if (!friendCompany) return;
-						
-						if (!nodes[friendCompany]) nodes[friendCompany] = {};
-						if (!nodes[friendCompany].persons) nodes[friendCompany].persons = [];
-						nodes[friendCompany].persons.push({name : friend.firstName + ' ' + friend.lastName, id:friend.id, title : friend.company ? friend.company.title : null});
+				});
+			}
 
-						if (nodes[friendCompany].persons.length > 1){
-							if (!edges[company]) edges[company] = [];
-							edges[company].push(friendCompany);
-						}
-					});
-				}
-				
-			});
+		}		
+		
+		
+		Person.findOne({id : req.query['id'] || app.currentUserId}, function(err, person){
 			
-			
-			
+			if (person){
+				// load the details and fill the node and edges variables		
+				fillPerson(person, 2);
+			}
 			res.json({nodes: nodes, edges : edges});
 		});
 
